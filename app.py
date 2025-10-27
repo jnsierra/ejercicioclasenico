4bec5c592e3057d849cd5b9cc115c8f862510811
Jesus Sierra <nicolas.sierra@fisagrp.com>
2025-10-27 17:59:22 -0500

    commit

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hola Mundo', 200
 
+@app.route('/startup', methods=['GET'])
+def startup():
+    return 'Startup OK', 200
+
+@app.route('/readiness', methods=['GET'])
+def readiness():
+    return 'Readiness OK', 200
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'Health OK', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
